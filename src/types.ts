ec9eedbcba29ad060d778f8d7ad5dc13993bba75
ty <towyuan@outlook.com>
2022-01-09 23:23:47 +0800

    chore: update overloads to support max 16 overloads functions infers

--- src/types.ts
+++ src/types.ts
@@ -16,7 +16,267 @@ type Overloads<T extends (...args: any[]) => any> = T extends {
   (...args: infer A4): infer R4;
   (...args: infer A5): infer R5;
   (...args: infer A6): infer R6;
+  (...args: infer A7): infer R7;
+  (...args: infer A8): infer R8;
+  (...args: infer A9): infer R9;
+  (...args: infer A10): infer R10;
+  (...args: infer A11): infer R11;
+  (...args: infer A12): infer R12;
+  (...args: infer A13): infer R13;
+  (...args: infer A14): infer R14;
+  (...args: infer A15): infer R15;
+  (...args: infer A16): infer R16;
 }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+      | ((...args: A10) => R10)
+      | ((...args: A11) => R11)
+      | ((...args: A12) => R12)
+      | ((...args: A13) => R13)
+      | ((...args: A14) => R14)
+      | ((...args: A15) => R15)
+      | ((...args: A16) => R16)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+      (...args: infer A9): infer R9;
+      (...args: infer A10): infer R10;
+      (...args: infer A11): infer R11;
+      (...args: infer A12): infer R12;
+      (...args: infer A13): infer R13;
+      (...args: infer A14): infer R14;
+      (...args: infer A15): infer R15;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+      | ((...args: A10) => R10)
+      | ((...args: A11) => R11)
+      | ((...args: A12) => R12)
+      | ((...args: A13) => R13)
+      | ((...args: A14) => R14)
+      | ((...args: A15) => R15)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+      (...args: infer A9): infer R9;
+      (...args: infer A10): infer R10;
+      (...args: infer A11): infer R11;
+      (...args: infer A12): infer R12;
+      (...args: infer A13): infer R13;
+      (...args: infer A14): infer R14;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+      | ((...args: A10) => R10)
+      | ((...args: A11) => R11)
+      | ((...args: A12) => R12)
+      | ((...args: A13) => R13)
+      | ((...args: A14) => R14)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+      (...args: infer A9): infer R9;
+      (...args: infer A10): infer R10;
+      (...args: infer A11): infer R11;
+      (...args: infer A12): infer R12;
+      (...args: infer A13): infer R13;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+      | ((...args: A10) => R10)
+      | ((...args: A11) => R11)
+      | ((...args: A12) => R12)
+      | ((...args: A13) => R13)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+      (...args: infer A9): infer R9;
+      (...args: infer A10): infer R10;
+      (...args: infer A11): infer R11;
+      (...args: infer A12): infer R12;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+      | ((...args: A10) => R10)
+      | ((...args: A11) => R11)
+      | ((...args: A12) => R12)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+      (...args: infer A9): infer R9;
+      (...args: infer A10): infer R10;
+      (...args: infer A11): infer R11;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+      | ((...args: A10) => R10)
+      | ((...args: A11) => R11)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+      (...args: infer A9): infer R9;
+      (...args: infer A10): infer R10;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+      | ((...args: A10) => R10)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+      (...args: infer A9): infer R9;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+      | ((...args: A9) => R9)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+      (...args: infer A8): infer R8;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+      | ((...args: A8) => R8)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+      (...args: infer A7): infer R7;
+    }
+  ?
+      | ((...args: A1) => R1)
+      | ((...args: A2) => R2)
+      | ((...args: A3) => R3)
+      | ((...args: A4) => R4)
+      | ((...args: A5) => R5)
+      | ((...args: A6) => R6)
+      | ((...args: A7) => R7)
+  : T extends {
+      (...args: infer A1): infer R1;
+      (...args: infer A2): infer R2;
+      (...args: infer A3): infer R3;
+      (...args: infer A4): infer R4;
+      (...args: infer A5): infer R5;
+      (...args: infer A6): infer R6;
+    }
   ?
       | ((...args: A1) => R1)
       | ((...args: A2) => R2)
